2f016be3e9d8b99fc6d12402b3304a15c42a2b06
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2023-06-06 15:57:37 -0400

    Add files via upload
    The program will create a CSV file where each row contains a sequence and its corresponding label. They will be labeled as 1 for positive case input and 0 for negative examples respectively. The CSV file will have two columns. The script can be run as follows: 
    python data_labeling.py <bed_file> <output_csv>

---
+++ data_labeling.py
@@ -0,0 +1,41 @@
+import sys
+import csv
+
+def create_csv(bed_file, output_csv):
+    sequences = []
+    labels = []
+
+    with open(bed_file, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('#'):
+                continue
+
+            fields = line.split('\t')
+            positive_sequence = fields[4]
+            negative_sequence = fields[5]
+
+            if positive_sequence:
+                sequences.append(positive_sequence)
+                labels.append('1')
+
+            if negative_sequence:
+                sequences.append(negative_sequence)
+                labels.append('0')
+
+    with open(output_csv, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Sequence', 'Label'])
+        writer.writerows(zip(sequences, labels))
+
+    print(f"CSV file created: {output_csv}")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: python create_csv.py <bed_file> <output_csv>")
+        sys.exit(1)
+
+    bed_file = sys.argv[1]
+    output_csv = sys.argv[2]
+
+    create_csv(bed_file, output_csv)
